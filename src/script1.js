3f96fc02cfb317607b753eef40dfd7f77663ab4d
Aleksei Ivanov <iao675@ya.ru>
2025-03-10 21:12:57 +0300

    Second

--- src/script1.js
+++ src/script1.js
@@ -2,4 +2,36 @@
 Develop by iao675 (Aleksei Ivanov)
 */
 "use strict"; //Включенее строго режима
-alert('iao675'); //Сообщение
+//alert('iao675'); //Сообщение
+//let num = 123; //Объявление переменной и присвоение значения (можно без присвоения)
+//num = 321 //Присвоение значения переменной
+//alert(num);
+//let q =1;
+//let w = 2;
+//let e = 3;
+//let a = 1, b = 2, c = 3;
+//let p, o, i;
+//p = 1;
+//o = 2;
+//i = 3;
+//let t;
+//t = 10;
+//alert(t);
+//t = 20;
+//alert(t);
+//let f = 1 + 2 + 3;
+//alert(f);
+//let h = 10;
+//let n = 2;
+//alert(h + n);
+//alert(h - n);
+//alert(h * n);
+//alert(h / n);
+//let k = 10;
+//let j = 5;
+//let result = k + j;
+//alert(result);
+//let a = 1, b = 2, c = 3;
+//alert(a + b + c);
+//let a = 10, b = 5, c = a - b, d = 7, reault = c + d;
+//alert(reault);
