7b50e695c028886141668d303f324d253cad0a9e
Aleksei Ivanov <iao675@ya.ru>
2025-03-19 08:35:48 +0300

    ⊗jsPmBsNPr

---
+++ src/prime/basis/script27.js
@@ -0,0 +1,33 @@
+/**
+ * Develop by iao675 (Aleksei Ivanov)
+ */
+"use strict";
+/**
+ * Тема 27
+ * ⊗jsPmBsNPr
+ */
+
+//#1
+//let a = 3 * 2 ** 3;
+//alert(a);
+//24
+
+//#2
+//let a = (3 * 2) ** 3;
+//alert(a);
+//216
+
+//#3
+//let a = 3 * 2 ** (3 +1);
+//alert(a);
+//48
+
+//#4
+//let a = 2 ** 3 * 3;
+//alert(a);
+//24
+
+//#5
+//let a = 3 * 2 ** 3 * 3;
+//alert(a);
+//72
