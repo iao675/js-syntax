e643a7041fa417304e075c83d0faebdb21761077
Aleksei Ivanov <iao675@ya.ru>
2025-03-19 09:56:50 +0300

    ⊗jsPmBsTS

---
+++ src/prime/basis/script31.js
@@ -0,0 +1,14 @@
+/**
+ * Develop by iao675 (Aleksei Ivanov)
+ */
+"use strict";
+/**
+ * Тема 31
+ * ⊗jsPmBsTS
+ */
+
+//#1
+//let str1 = 'xxx';
+//let str2 = 'yyy';
+//let txt = `aaa ${str1} bbb ${str2} ccc`;
+//alert(txt);
