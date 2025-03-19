d3d7836c2cde0ec4e37d4f98677980c031d95a22
Aleksei Ivanov <iao675@ya.ru>
2025-03-19 09:43:34 +0300

    ⊗jsPmBsStr

---
+++ src/prime/basis/script29.js
@@ -0,0 +1,22 @@
+/**
+ * Develop by iao675 (Aleksei Ivanov)
+ */
+"use strict";
+/**
+ * Тема 28
+ * ⊗jsPmBsStr
+ */
+
+//#1
+//let str = '!!!';
+//alert(str);
+
+//#2
+//let java = 'java';
+//let script = 'script';
+//alert(java + script);
+
+//#3
+//let a = 'hello';
+//let b = 'world';
+//alert(a + ' ' + b);
