e048e0c714acfc44ed930e3c3e4292444ca47c16
Aleksei Ivanov <iao675@ya.ru>
2025-03-19 08:46:03 +0300

    ⊗jsPmBsStr

---
+++ src/prime/basis/script28.js
@@ -0,0 +1,14 @@
+/**
+ * Develop by iao675 (Aleksei Ivanov)
+ */
+"use strict";
+/**
+ * Тема 28
+ * ⊗jsPmBsStr
+ */
+
+//#1
+//let firstname = 'Aleksei';
+//let lastname = 'Ivanov';
+//alert(firstname);
+//alert(lastname);
